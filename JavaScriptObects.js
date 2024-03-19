8ae6e40f7d779ed4921add123978f676e63fb55b
Alexander <StevenAlexanderDraai@gmail.com>
2024-03-19 14:05:25 +0200

    Updated WebDev Tut.

--- JavaScriptObects.js
+++ JavaScriptObects.js
@@ -11,7 +11,7 @@ const objAlex = {
     age       : 23,
     isEmployed: false,
     sayHello  :  (firstName) => {console.log(`My name is ${firstName}`)},
-    eat       :  ()  => {console.log(`I am eating susji.`)} 
+    eat       :  ()  => {console.log(`I am eating sushi.`)}
 } 
 
 const objTia = {
@@ -31,10 +31,10 @@ const objTia = {
 
 
 
-// THIS - reference to the obkect where THIS is used. 
+// THIS - reference to the object where THIS is used.
 
-const objSpongebog = {
-    name : "Spongebog", 
+const objSpongeBob = {
+    name : "SpongeBob",
     favFood : "Hamburgers", 
     sayHello: function() {console.log(`hi I am ${this.name}`)},
     eat:    function() {console.log(`${this.name} is eating ${this.favFood}`)},
@@ -47,7 +47,7 @@ const objPatrick = {
     eat:    function() {console.log(`${this.name} is eating ${this.favFood}`)},
 }
 
-//objSpongebog.eat();
+//objSpongeBob.eat();
 //objPatrick.eat();
 
 // Constructors !
@@ -71,7 +71,7 @@ const carVW = new Car("VW", "GOLF 8", 2024, "BLACK");
 //carVW.drive();
 
 
-// Classes - provide structured and cleanere way to work with objects. 
+// Classes - provide structured and cleaner way to work with objects.
 
 class Products{
     constructor(name, price){
@@ -110,7 +110,7 @@ const totalProductThree = product3.calcTotal(salesTax);
 //console.log(`Total Price (with tax): $${totalProductThree.toFixed(2)}`);
 
 
-// Static - keyword that defines PROPERTIES or METHODS that belong to a class itself rathe rthan the objects created from that class. 
+// Static - keyword that defines PROPERTIES or METHODS that belong to a class itself rather than the objects created from that class.
 
 class MathUtil{
     static PI = 3.14159 
@@ -159,9 +159,149 @@ const userThree = new User("Mo");
 //console.log(User.userCount);
 //User.getUserCount();
 
-// Inheritance - allows classes to inherit properties and methods from an existing class (partent -> child)  
+// Inheritance - allows classes to inherit properties and methods from an existing class (parent -> child)
+// SUPER - keyword - used in all classes to call constructor OR access the properties & methods of a parent class.
+// this = object
+// super = parent
 
-class P{}
+class Animal{
+  constructor(name, age) {
+      this.name = name;
+      this.age = age;
+  }
+
+  move(speed){
+      console.log(`The ${this.name} moves at a speed of ${speed}mph.`)
+  }
+}
+class Rabbit extends Animal{
+  constructor(name, age, runSpeed) {
+      super(name, age);
+      this.runSpeed = runSpeed;
+  }
+
+  run(){
+      console.log(`This ${this.name} can run`);
+      super.move(this.runSpeed);
+  }
+}
+class Fish extends Animal{
+   constructor(name, age, swimSpeed) {
+       super(name, age);
+       this.swimSpeed = swimSpeed;
+   }
+    run(){
+        console.log(`This ${this.name} can swim`);
+        super.move(this.swimSpeed);
+    }
+}
+class Hawk extends Animal{
+   constructor(name, age, flySpeed) {
+       super(name, age);
+       this.flySpeed = flySpeed;
+   }
+    run(){
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
+    }
+}
+
+const rabbit = new Rabbit(`Alex`,21,154);
+const fish = new Fish(`Jackson `,76,200);
+const hawk = new Hawk(`Milo`,51,124);
+
+// Getters and Setters
+
+class Rectangle{
+    constructor(width, height) {
+        this.height = height;
+        this._width = width;
+    }
+    set width(newWidth) {
+        if(newWidth > 0){
+            this._width = newWidth;
+        } else  {
+            console.log(`Width must be a positive number.`);
+        }
+    }
+
+    set height(newHeight) {
+        if(newHeight > 0){
+            this._height = newHeight;
+        } else  {
+            console.log(`Height must be a positive number.`);
+        }
+    }
+    get width(){
+        return `${this._width.toFixed(1)}cm`
+            ;}
+    get height(){
+        return `${this._height.toFixed(1)}cm`
+            ;}
+
+    get area(){return `${(this._height * this._width).toFixed(1)}cm`};
+
+}
+const rectangle = new Rectangle(5, 10);
+rectangle.height = 100;
+rectangle.width = 100;
+
+//console.log(rectangle.height);
+//console.log(rectangle.width);
+//console.log(rectangle.area);
 
 
-class PD{}
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName(newFirstName){
+        if(typeof newFirstName === "string" && newFirstName.length > 0){
+            this._firstName = newFirstName;
+        } else {
+            console.error("First name must be a non-empty string");
+        }
+    }
+
+    set lastName(newLastName){
+        if(typeof newLastName === "string" && newLastName.length > 0){
+            this._lastName = newLastName;
+        } else {
+            console.error("Last name must be a non-empty string");
+        }
+    }
+
+    set age(newAge){
+        if(typeof newAge === "number" && newAge >= 0){
+            this._age = newAge;
+        } else {
+            console.error("age must be a non-negative number");
+        }
+    }
+
+    get firstName(){return this._firstName};
+    get lastName(){return this._lastName};
+
+    get fullName(){return `${this._firstName} ${this._lastName}`};
+
+    get age(){return this._age};
+}
+
+const person = new Person("Alexander","Draai",23);
+
+//console.log(person.firstName);
+//console.log(person.lastName);
+//console.log(person.fullName);
+//console.log(person.age);
+
+
+// Destructuring -> extract values from arrays and objects, then assign them to variables in a convenient way !
+/*
+    [] = to perform array destructing
+    {} - to perform object destructing
+ */
+
+
