05c7a10d32a4a7ca3953f30fd8201885a69c03ce
Alexander <StevenAlexanderDraai@gmail.com>
2024-03-23 23:11:39 +0200

    okay I am starting to get a hang of all of this.

--- JavaScript Scripts/Promises.js
+++ JavaScript Scripts/Promises.js
@@ -6,31 +6,74 @@
 
 
 // .1 Walk the dog
-function walkDog(callback){
-    setTimeout(() => {
-        console.log(`You walk the dog.`);
-        callback();
-        }
-        ,1000);
+function walkDog(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dogWalked = true;
+            if (dogWalked) {
+                resolve(`You walk the dog 🐕`);
+            } else {
+                reject("You didn't walk the dog");
+            }
+             // Completion message.
+        },1000);
+    });
 }
 
 // .2 Clean the kitchen
-function cleanKitchen(callback){
-    setTimeout(() => {
-            console.log(`You cleaned the kitcen.`);
-            callback();
-            },2500);
+function cleanKitchen(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const kitchenCleaned = true;
+            if (kitchenCleaned) {
+                resolve(`You cleaned the kitchen 🧹`);
+            } else {
+                reject("You did not clean the kitchen.");
+            }
+        },2000);
+    });
 }
 
 // .3 Take out the trash.
 function takeOutTrash(){
-    setTimeout(() => {
-            console.log(`You take out the trash.`);}
-        ,500);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const trashTakenOut = true ;
+            if (trashTakenOut) {
+                resolve(`You take out the trash 🗑️`);
+            } else {
+                reject("You did not take out the trash.")
+            }
+        },500);
+    })
 }
 
-walkDog(() =>{
+
+walkDog().then(value => {console.log(value);return cleanKitchen()})
+         .then(value => {console.log(value); return takeOutTrash()})
+         .then(value => {console.log(value); console.log("All the chores are done mate.")})
+         .catch((error) => {console.log(error)});
+
+
+
+async function doChores(){
+    walkDogResult = await walkDog();
+    console.log(walkDogResult);
+
+    const cleanKitchenResult = await cleanKitchen();
+    console.log(cleanKitchenResult);
+
+    const takeOutTrashResult = await takeOutTrash();
+    console.log(takeOutTrashResult);
+
+    console.log("You finished all the chores.");
+}
+
+//!  Callback hell !
+/*
+walkDog( () => {
     cleanKitchen(() => {
-        takeOutTrash(() => console.log('you have finished all your chores mate.'));
+        takeOutTrash(() => console.log("Chores are done mate."));
     })
 });
+ */
